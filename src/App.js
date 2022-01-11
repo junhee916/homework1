89d9d35c54fe21a9ee4e63087aa4ae67dd3cf0d3
junhee0916 <junhee916@naver.com>
2022-01-11 17:53:00 +0900

    react_state_basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+/* eslint-disable*/
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  let [title, changeTitle] = useState(['test1', 'test2', 'test3']);
+  let [like, changeLike] = useState(0);
+  function resultTitle(){
+    var newArray = [...title]
+    newArray[0] = '여자코트 추천'
+    changeTitle(newArray)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={resultTitle}>버튼</button>
+        <div>
+          <h1>{title[0]}<span onClick={() => {changeLike(like+1)}}>👍</span>{like}</h1>
+          <div>2022-01-01</div>
+        </div>
+        <div>
+          <h1>{title[1]}</h1>
+          <div>2022-01-02</div>
+        </div>
+        <div>
+          <h1>{title[2]}</h1>
+          <div>2022-01-03</div>
+        </div>
+
+        <Modal/>
+      </div>
     </div>
   );
 }
 
+function Modal(){
+  return(
+    <div className='modal'>
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+  </div>
+  )
+}
 export default App;
